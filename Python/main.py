8d183b5baae6496023dba9ea180cfde8072a291c
Hsury <i@hsury.com>
2017-10-04 15:05:06 +0800

    Use MultiThreading to handle network request

--- Python/main.py
+++ Python/main.py
@@ -2,23 +2,18 @@ from PyQt5 import QtCore, QtWidgets
 from qtWindow import Ui_MainWindow
 from time import sleep
 import sys
+import threading
 import requests
 import json
 
 talking = False
 
-class ApplicationWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(ApplicationWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.connectButton.clicked.connect(self.connectESP8266)
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.wndUpdate)
-    
-    def wndUpdate(self):
+def getData():
+    global ipAddr, talking, ping
+    global mac, version, time, temp, pwm, border
+    while talking:
         try:
-            esp8266 = requests.get(ipAddr, timeout = 0.2)
+            esp8266 = requests.get(ipAddr, timeout = 0.1)
             jsondata = esp8266.json()
             mac = jsondata['mac']
             version = jsondata['version']
@@ -27,9 +22,22 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             pwm = jsondata['pwm']
             border = jsondata['border']
             ping = True
-        except :
+        except:
             ping = False
-        if (ping == True):
+        sleep(0.1)
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.connectButton.clicked.connect(self.connect)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.wndUpdate)
+    
+    def wndUpdate(self):
+        global ping
+        if ping:
             self.ui.tempLCD.display(temp)
             self.ui.pwmLCD.display(pwm)
             self.ui.borderLLCD.display(border[0])
@@ -37,16 +45,22 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             self.ui.mac.setText('MAC: ' + mac)
             self.ui.version.setText('版本: ' + version)
     
-    def connectESP8266(self):
+    def connect(self):
         global ipAddr, talking
         talking = not talking
-        if (talking == True):
-            self.timer.start(250)
+        if talking:
+            self.timer.start(100)
             ipAddr = 'http://'\
                      + self.ui.ipSeg1.text() + '.'\
                      + self.ui.ipSeg2.text() + '.'\
                      + self.ui.ipSeg3.text() + '.'\
                      + self.ui.ipSeg4.text()
+            try:
+                global thd
+                thd = threading.Thread(target = getData)
+                thd.start()
+            except:
+                pass
             self.ui.ipSeg1.setEnabled(False)
             self.ui.ipSeg2.setEnabled(False)
             self.ui.ipSeg3.setEnabled(False)
@@ -60,6 +74,10 @@ class ApplicationWindow(QtWidgets.QMainWindow):
             self.ui.ipSeg4.setEnabled(True)
             self.ui.connectButton.setText('连接')
     
+    def closeEvent(self, event):
+        global talking
+        talking = False
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     application = ApplicationWindow()
