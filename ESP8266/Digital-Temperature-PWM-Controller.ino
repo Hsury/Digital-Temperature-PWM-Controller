c08f5c018290f1c72a5eef0ad50978ebd9075070
Hsury <i@hsury.com>
2017-10-22 04:01:03 +0800

    Add manually fitting and many other functions

--- ESP8266/Digital-Temperature-PWM-Controller.ino
+++ ESP8266/Digital-Temperature-PWM-Controller.ino
@@ -4,38 +4,63 @@
    Website : http://hsury.com/
 */
 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <WiFiUdp.h>
-#include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <NTPClient.h>
-#include <EEPROM.h>
-#include <ArduinoJson.h>
+#define BORDER_L 30
+#define BORDER_H 50
+#define FITTING_K 100 / 1024.0
+#define FITTING_B 40
+#define SAMPLING_PERIOD 250
 
-const char* ver = "20171006";
-const char* update_path = "/ota";
+#include <ArduinoJson.h>
+#include <ArduinoOTA.h>
+#include <EEPROM.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <SevenSegmentTM1637.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+
+const byte pinCLK = 4;
+const byte pinDIO = 5;
+const char* password = "KAGAMIZ.COM";
+const char* ssid = "HsuRY";
 const char* update_username = "ILWT";
 const char* update_password = "ILWT";
-const char* ssid = "Xiaomi_33C7";
-const char* password = "duoguanriben8";
-const String cmdList[5] = {"restart", "restore", "setBorder", "setOffset", "serialPrint"};
+const char* update_path = "/ota";
+const char* ver = "20171022";
+const String cmdList[5] = {"border", "fitting", "restart", "restore", "print"};
 
 boolean isOnline;
-unsigned int localUdpPort = 8266;
+byte menu;
 char incomingPacket[255];
-char  replyPacekt[] = "Got it, ILWT!\n";
-float temp, offset;
+char replyPacekt[] = "Got it, ILWT!\n";
+float border[2], fitting[2], temp;
+int adc, pwm;
 String tempStr;
-int pwm, border[2];
+unsigned int localUdpPort = 8266;
 unsigned long timeStamp[2];
 
-WiFiUDP mainUDP, ntpUDP;
-ESP8266WebServer server(80);
 ESP8266HTTPUpdateServer httpUpdater;
+ESP8266WebServer server(80);
+WiFiUDP mainUDP, ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp1.aliyun.com", 28800, 10000);
+SevenSegmentTM1637 display(pinCLK, pinDIO);
+
+void stateMachine(void) {
+  switch (menu) {
+    case 0:
+      byte tempStrLen = tempStr.length();
+      byte  rawData[4];
+      rawData[0] = display.encode(tempStrLen > 4 ? tempStr[tempStrLen - 5] : ' ');
+      rawData[1] = display.encode(tempStrLen > 3 ? tempStr[tempStrLen - 4] : ' ');
+      rawData[2] = display.encode(tempStr[tempStrLen - 3]) | B10000000;
+      rawData[3] = display.encode(tempStr[tempStrLen - 1]);
+      display.printRaw(rawData);
+      break;
+  }
+}
 
 void bannerPrint(void) {
   Serial.println();
@@ -47,16 +72,22 @@ void bannerPrint(void) {
 }
 
 void readConf(void) {
-  EEPROM.begin(4);
+  EEPROM.begin(9);
   if (EEPROM.read(0) != 0x39) {
     EEPROM.write(0, 0x39);
-    EEPROM.write(1, 0);
-    EEPROM.write(2, 30);
-    EEPROM.write(3, 50);
+    EEPROM.write(1, round(BORDER_L * 10) / 10);
+    EEPROM.write(2, round(BORDER_L * 10) % 10);
+    EEPROM.write(3, round(BORDER_H * 10) / 10);
+    EEPROM.write(4, round(BORDER_H * 10) % 10);
+    EEPROM.write(5, round(FITTING_K * 100) / 100);
+    EEPROM.write(6, round(FITTING_K * 100) % 100);
+    EEPROM.write(7, round(FITTING_B * 100) / 100);
+    EEPROM.write(8, round(FITTING_B * 100) % 100);
   }
-  offset = EEPROM.read(1);
-  border[0] = EEPROM.read(2);
-  border[1] = EEPROM.read(3);
+  border[0] = EEPROM.read(1) + EEPROM.read(2) * 0.1;
+  border[1] = EEPROM.read(3) + EEPROM.read(4) * 0.1;
+  fitting[0] = EEPROM.read(5) + EEPROM.read(6) * 0.01;
+  fitting[1] = EEPROM.read(7) + EEPROM.read(8) * 0.01;
   EEPROM.end();
 }
 
@@ -103,6 +134,9 @@ boolean connectWifi(void) {
   Serial.printf("Connecting to %s ", ssid);
   for (int i = 0; i < 60; i++) {
     delay(500);
+    char statusbar[] = "    ";
+    statusbar[i % 4] = '-';
+    display.print(statusbar);
     if (WiFi.status() != WL_CONNECTED) {
       Serial.print(">");
     } else {
@@ -127,17 +161,24 @@ boolean connectWifi(void) {
     Serial.println("HTTP server started");
     timeClient.begin();
     Serial.println("NTP client started");
+    display.print("PASS");
+    delay(500);
+    Serial.println("Displaying IP address");
+    display.printf("   %d.%d.%d.%d   ", WiFi.localIP()[0], WiFi.localIP()[1], WiFi.localIP()[2], WiFi.localIP()[3]);
     return true;
   } else {
     Serial.println("FAIL");
     WiFi.disconnect(true);
+    display.print("FAIL");
+    delay(500);
     return false;
   }
 }
 
 void dataProcess(void) {
-  if (timeStamp[0] == 0 or millis() - timeStamp[0] > 250) {
-    temp = analogRead(A0) / 1024.0 * 100 + offset;
+  if (timeStamp[0] == 0 or millis() - timeStamp[0] > SAMPLING_PERIOD) {
+    adc = analogRead(A0);
+    temp = fitting[0] * adc + fitting[1];
     if (temp < 0) temp = 0;
     if (temp > 100) temp = 100;
     tempStr = String(int(temp * 10 + 0.5) / 10.0);
@@ -151,7 +192,7 @@ void dataProcess(void) {
 
 void handleRoot(void) {
   String msg;
-  StaticJsonBuffer<256> jsonBuffer;
+  DynamicJsonBuffer jsonBuffer;
   JsonObject& json = prepareResponse(jsonBuffer);
   json.prettyPrintTo(msg);
   server.send(200, "application/json", msg);
@@ -159,14 +200,18 @@ void handleRoot(void) {
 
 JsonObject& prepareResponse(JsonBuffer& jsonBuffer) {
   JsonObject& root = jsonBuffer.createObject();
-  root["mac"] = getMAC();
-  root["version"] = ver;
-  root["time"] = timeClient.getFormattedTime();
-  root["temp"] = tempStr;
-  root["pwm"] = pwm;
+  root["adc"] = adc;
   JsonArray& borderJson = root.createNestedArray("border");
   borderJson.add(border[0]);
   borderJson.add(border[1]);
+  JsonArray& fittingJson = root.createNestedArray("fitting");
+  fittingJson.add(fitting[0]);
+  fittingJson.add(fitting[1]);
+  root["mac"] = getMAC();
+  root["pwm"] = pwm;
+  root["temp"] = tempStr;
+  root["time"] = timeClient.getFormattedTime();
+  root["version"] = ver;
   return root;
 }
 
@@ -208,52 +253,95 @@ void cmdCheck(char* packet) {
       Serial.println();
       switch (i) {
         case 0:
-          Serial.println("Restarting...");
-          ESP.restart();
-          break;
-        case 1:
-          EEPROM.begin(4);
-          EEPROM.write(0, 0x39);
-          EEPROM.write(1, 0);
-          EEPROM.write(2, 30);
-          EEPROM.write(3, 50);
-          offset = EEPROM.read(1);
-          Serial.print("offset <= ");
-          Serial.println(offset);
-          border[0] = EEPROM.read(2);
-          border[1] = EEPROM.read(3);
-          Serial.printf("border <= {%d, %d}", border[0], border[1]);
-          Serial.println();
-          EEPROM.end();
-          break;
-        case 2:
           if (parameter.indexOf(',') != -1) {
-            unsigned int newBorder[2] = {parameter.substring(0, parameter.indexOf(',')).toInt(), parameter.substring(parameter.indexOf(',') + 1, parameter.length()).toInt()};
-            if (newBorder[0] >= 0 and newBorder[0] < newBorder[1] and newBorder[1] <= 100) {
-              border[0] = newBorder[0];
-              border[1] = newBorder[1];
-              EEPROM.begin(4);
-              EEPROM.write(2, border[0]);
-              EEPROM.write(3, border[1]);
+            float newBorder[2] = {parameter.substring(0, parameter.indexOf(',')).toFloat(), parameter.substring(parameter.indexOf(',') + 1, parameter.length()).toFloat()};
+            if (newBorder[0] > 0 and newBorder[0] < newBorder[1] and newBorder[1] < 100 ) {
+              EEPROM.begin(9);
+              EEPROM.write(1, round(newBorder[0] * 10) / 10);
+              EEPROM.write(2, round(newBorder[0] * 10) % 10);
+              EEPROM.write(3, round(newBorder[1] * 10) / 10);
+              EEPROM.write(4, round(newBorder[1] * 10) % 10);
+              border[0] = EEPROM.read(1) + EEPROM.read(2) * 0.1;
+              border[1] = EEPROM.read(3) + EEPROM.read(4) * 0.1;
               EEPROM.end();
-              Serial.printf("border <= {%d, %d}", border[0], border[1]);
-              Serial.println();
+              Serial.print("border <= {");
+              Serial.print(border[0]);
+              Serial.print(", ");
+              Serial.print(border[1]);
+              Serial.println("}");
             } else Serial.println("Value error");
           } else Serial.println("Format error");
           break;
+        case 1:
+          if (parameter.indexOf(',') != -1) {
+            float newFitting[2] = {parameter.substring(0, parameter.indexOf(',')).toFloat(), parameter.substring(parameter.indexOf(',') + 1, parameter.length()).toFloat()};
+            EEPROM.begin(9);
+            EEPROM.write(5, round(newFitting[0] * 100) / 100);
+            EEPROM.write(6, round(newFitting[0] * 100) % 100);
+            EEPROM.write(7, round(newFitting[1] * 100) / 100);
+            EEPROM.write(8, round(newFitting[1] * 100) % 100);
+            fitting[0] = EEPROM.read(5) + EEPROM.read(6) * 0.01;
+            fitting[1] = EEPROM.read(7) + EEPROM.read(8) * 0.01;
+            EEPROM.end();
+            Serial.print("fitting <= {");
+            Serial.print(fitting[0]);
+            Serial.print(", ");
+            Serial.print(fitting[1]);
+            Serial.println("}");
+          } else Serial.println("Format error");
+          break;
+        case 2:
+          Serial.println("Restarting...");
+          ESP.restart();
+          break;
         case 3:
-          offset = parameter.toFloat();
-          EEPROM.begin(4);
-          EEPROM.write(1, round(offset));
+          EEPROM.begin(9);
+          EEPROM.write(0, 0x39);
+          EEPROM.write(1, round(BORDER_L * 10) / 10);
+          EEPROM.write(2, round(BORDER_L * 10) % 10);
+          EEPROM.write(3, round(BORDER_H * 10) / 10);
+          EEPROM.write(4, round(BORDER_H * 10) % 10);
+          EEPROM.write(5, round(FITTING_K * 100) / 100);
+          EEPROM.write(6, round(FITTING_K * 100) % 100);
+          EEPROM.write(7, round(FITTING_B * 100) / 100);
+          EEPROM.write(8, round(FITTING_B * 100) % 100);
+          border[0] = EEPROM.read(1) + EEPROM.read(2) * 0.1;
+          border[1] = EEPROM.read(3) + EEPROM.read(4) * 0.1;
+          fitting[0] = EEPROM.read(5) + EEPROM.read(6) * 0.01;
+          fitting[1] = EEPROM.read(7) + EEPROM.read(8) * 0.01;
           EEPROM.end();
-          Serial.print("offset <= ");
-          Serial.println(offset);
+          Serial.print("border <= {");
+          Serial.print(border[0]);
+          Serial.print(", ");
+          Serial.print(border[1]);
+          Serial.println("}");
+          Serial.print("fitting <= {");
+          Serial.print(fitting[0]);
+          Serial.print(", ");
+          Serial.print(fitting[1]);
+          Serial.println("}");
           break;
         case 4:
-          Serial.printf("Temperature: %s, PWM: %d, Offset: ", tempStr.c_str(), pwm);
-          Serial.print(offset);
-          Serial.printf(", Border: {%d, %d}", border[0], border[1]);
+          Serial.println("---------- BEGIN ----------");
+          Serial.printf("ADC = %d", adc);
           Serial.println();
+          Serial.print("Border = {");
+          Serial.print(border[0]);
+          Serial.print(", ");
+          Serial.print(border[1]);
+          Serial.println("}");
+          Serial.print("Fitting = {");
+          Serial.print(fitting[0]);
+          Serial.print(", ");
+          Serial.print(fitting[1]);
+          Serial.println("}");
+          Serial.printf("PWM = %d", pwm);
+          Serial.println();
+          Serial.printf("Temperature = %s", tempStr.c_str());
+          Serial.println();
+          Serial.printf("Time = %s", timeClient.getFormattedTime().c_str());
+          Serial.println();
+          Serial.println("----------- END -----------");
           break;
       }
       break;
@@ -261,65 +349,22 @@ void cmdCheck(char* packet) {
   }
 }
 
-/*
-  boolean ssd4Drv(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t invl, unsigned long& timeStamp, char* str) {
-  String text = String(str);
-  int ptrBegin = (text.length() > 4) ? -3 : 0;
-  int ptrEnd = (text.length() > 4) ? text.length() - 1 : 0;
-  int i = ptrBegin;
-  for (int j = 0; j++; j < 4) {
-    uint8_t led;
-    if (i + j >= 0 and i + j <= text.length() - 1) {
-      switch (text[i + j]) {
-        case '.':
-          led = B00000001;
-          break;
-
-        case 'I':
-          led = B01100000;
-          break;
-
-        case 'P':
-          led = B11001110;
-          break;
-
-        default:
-          led = 0;
-          break;
-      }
-      if (!led & B00000001 and i + j + 1 >= 0 and i + j + 1 <= text.length() - 1 and text[i + j + 1] == '.') {
-        led |= B00000001;
-        i++;
-      }
-    } else {
-      led = 0;
-    }
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, led);
-    shiftOut(dataPin, clockPin, LSBFIRST, B10000000 << j);
-    digitalWrite(latchPin, HIGH);
-  }
-  if (timeStamp == 0 or millis() - timeStamp > invl) {
-    i++;
-    if (i >= ptrEnd + 1) {
-      i = ptrBegin;
-    }
-    timeStamp = millis();
-  }
-  }
-*/
-
 void setup(void) {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  display.setPrintDelay(200);
+  display.begin();
+  display.print("boot");
   bannerPrint();
   readConf();
   isOnline = connectWifi();
+  Serial.println("All done");
 }
 
 void loop(void) {
   digitalWrite(LED_BUILTIN, LOW);
   dataProcess();
+  stateMachine();
   if (isOnline) {
     udpHandle(mainUDP);
     ArduinoOTA.handle();
